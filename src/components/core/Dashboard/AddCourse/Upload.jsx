30d17509b25ed79065f36c0951484b7d73bc852b
Kratik Mujawdiya <144536887+KratikMujawdiya@users.noreply.github.com>
2026-02-08 00:18:53 +0530

    Update Upload.jsx

--- src/components/core/Dashboard/AddCourse/Upload.jsx
+++ src/components/core/Dashboard/AddCourse/Upload.jsx
@@ -8,6 +8,7 @@ import { Player } from "video-react"
 
 export default function Upload({
     disabled,
+    id,//add by me
     name,
     label,
     register,
@@ -56,8 +57,9 @@ export default function Upload({
     }, [selectedFile, setValue])
 
     return (
+        //htmlFor{name} ki jagah htmlFor={id}
         <div className="flex flex-col space-y-2">
-            <label className="text-sm text-richblack-100 px-1" htmlFor={name}>
+            <label className="text-sm text-richblack-100 px-1" htmlFor={id}>
                 {label} {!viewData && <sup className="text-pink-200">*</sup>}
             </label>
             <div
@@ -95,7 +97,12 @@ export default function Upload({
                         className="flex w-full flex-col items-center p-6"
                         {...getRootProps()}
                     >
-                        <input {...getInputProps()} ref={inputRef} />
+                        <input
+                          {...getInputProps()}
+                          ref={inputRef}
+                          id={id}        
+                          name={name}   
+                        />
                             <div className="grid aspect-square w-16 place-items-center rounded-full bg-pure-greys-800">
                             <FiUploadCloud className="text-2xl text-yellow-50" />
                         </div>
